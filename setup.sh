772ca337af8f11e48c5502f85351bc05ee2ef18e
shah-meet0 <flario.ms@gmail.com>
2021-10-04 16:39:43 +0100

    added flags

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]
+base = 'light'
+primaryColor='#6eb52f'
+backgroundColor='#f0f0f5'
+secondaryBackgroundColor='#e0e0ef'
+textColor='#262730'
+font='sans serif'
 " > ~/.streamlit/config.toml
