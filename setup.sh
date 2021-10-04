03e545770eb145c8805e7d5c347b70b78e4eff53
Meet Shah <66689105+shah-meet0@users.noreply.github.com>
2021-10-04 13:31:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo '\
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-
-[theme]
-
-base = "light"
-primaryColor="#6eb52f"
-backgroundColor="#f0f0f5"
-secondaryBackgroundColor="#e0e0ef"
-textColor="#262730"
-font="sans serif"
-
-' > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
